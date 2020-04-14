3ead31a4fd25deafd1eebc9b6e4c19b4839948e2
Isaac Kyalo <isaacinkboy@gmail.com>
2020-04-14 19:33:23 +0300

    Challenge1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,9 @@
 const covid19ImpactEstimator = (data) => data;
 
+currentlyInfectedImpact = covid19ImpactEstimator.reportedCases*10;
+impact.currentlyInfected = currentlyInfectedImpact;
+currentlyInfectedSevere = covid19ImpactEstimator.reportedCases * 50;
+severeImpact.currentlyInfected = currentlyInfectedSevere;
+impact.infectionsByRequestedTime= currentlyInfectedImpact * 1024;
+severeImpact.infectionsByRequestedTime = currentlyInfectedSevere * 1024;
 export default covid19ImpactEstimator;
